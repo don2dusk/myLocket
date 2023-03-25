35813db781ba5f11470b465da1fbac4ecad7c56e
don2dusk <otegabrotobor@gmail.com>
2023-03-25 18:52:04 +0100

    main page with chat_list and profile

---
+++ lib/chats/chat_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatsList extends StatefulWidget {
+  const ChatsList({super.key});
+
+  @override
+  State<ChatsList> createState() => _ChatsListState();
+}
+
+class _ChatsListState extends State<ChatsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
